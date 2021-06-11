7176eba3c91949339bb6ae2336fe9b680c119660
David Cruz <dach_092@hotmail.com>
2021-06-11 15:04:10 -0500

    Subiendo cambios.

--- api-reto/src/main/infrastructure/app.js
+++ api-reto/src/main/infrastructure/app.js
@@ -1,10 +1,12 @@
 "use strict";
 const Koa = require('koa');
+const bodyParser =  require('koa-bodyparser');
 const routes = require('./routers/index');
 
 const app = new Koa();
 
 app.use(routes);
+app.use(bodyParser());
 
 module.exports = app;
 
